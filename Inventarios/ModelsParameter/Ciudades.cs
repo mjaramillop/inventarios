97ce92b0ad6dcfa941190912abc0bef41b50ccd7
MAURICIO <mauriciojaramillopardo@gmail.com>
2023-11-04 18:10:14 -0500

    crear entidad ciudades como parametro de entrada

---
+++ Inventarios/ModelsParameter/Ciudades.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace Inventarios.ModelsParameter;
+
+public partial class Ciudades
+{
+    public int? id { get; set; } = null!;
+
+    public string? codigo1 { get; set; }
+    public string? nivel1 { get; set; }
+    public string? codigo2 { get; set; }
+    public string? nivel2 { get; set; }
+    public string? codigo3 { get; set; }
+    public string? nivel3 { get; set; }
+    public string? codigo4 { get; set; }
+    public string? nivel4 { get; set; }
+    public string? codigo5 { get; set; }
+    public string? nivel5 { get; set; }
+
+
+    public string? codigo1nuevo { get; set; }
+    public string? nivel1nuevo { get; set; }
+    public string? codigo2nuevo { get; set; }
+    public string? nivel2nuevo { get; set; }
+    public string? codigo3nuevo { get; set; }
+    public string? nivel3nuevo { get; set; }
+    public string? codigo4nuevo { get; set; }
+    public string? nivel4nuevo { get; set; }
+    public string? codigo5nuevo { get; set; }
+    public string? nivel5nuevo { get; set; }
+
+
+
+
+
+    public string? estadodelregistro { get; set; }
+
+   
+}
