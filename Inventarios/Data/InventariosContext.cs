a3c7680ababbb75661e9d0032dfc8d34cf181a01
mauricio <mauriciojaramillopardo@gmail.com>
2024-08-02 22:21:26 -0500

    SE CAMBIA LA CONEXCION YA NO SIRVE EN EL JOSON APPSETTINGS

--- Inventarios/Data/InventariosContext.cs
+++ Inventarios/Data/InventariosContext.cs
@@ -72,11 +72,14 @@ public partial class InventariosContext : DbContext
 
     public virtual DbSet<FechaDeCierre> FechaDeCierre { get; set; }
 
-    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //    => optionsBuilder.UseSqlServer("Server=DESKTOP-56QVMV6\\SQLEXPRESS;Database=inventarios;Trusted_Connection=True;TrustServerCertificate=true;");
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer(_iconfiguration.GetConnectionString("connectionstring"));
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+    => optionsBuilder.UseSqlServer("Server=DESKTOP-2DAGVI9\\SQLEXPRESS2;Database=inventarios;Trusted_Connection=True;TrustServerCertificate=true");
+
+
+   // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    => optionsBuilder.UseSqlServer(_iconfiguration.GetConnectionString("connectionstring"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
